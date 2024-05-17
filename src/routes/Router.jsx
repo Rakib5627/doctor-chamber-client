f91fd390e7c717310d5135b8fbeb099d51ca9fa8
Rakibul Islam <mdrakib.5627@gmail.com>
2024-05-18 00:36:25 +0600

    project setup and navbar and banner

---
+++ src/routes/Router.jsx
@@ -0,0 +1,22 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../components/pages/Home/Home";
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+    //   errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+    ]}
+    
+    
+
+  ]);
